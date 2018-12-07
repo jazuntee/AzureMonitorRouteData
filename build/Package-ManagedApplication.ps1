92ba98723802a9904d24cb2b03c0f51f02ac6169
Jason Thompson (ID CXP) <jasoth@microsoft.com>
2018-12-07 18:18:41 -0500

    Update

---
+++ build/Package-ManagedApplication.ps1
@@ -0,0 +1,33 @@
+param(
+    #
+    [Parameter(Mandatory=$false)]
+    [string] $StagingDirectory = ".\release\staging",
+    #
+    [Parameter(Mandatory=$false)]
+    [string] $PackagePath = ".\release\AzureMonitorRouteData.zip"
+)
+
+## Convert relative paths to absolute
+if (![System.IO.Path]::IsPathRooted($StagingDirectory)) {
+    $StagingDirectory = [System.IO.Path]::GetFullPath((Join-Path (Get-Location).ProviderPath $StagingDirectory))
+}
+if (![System.IO.Path]::IsPathRooted($PackagePath)) {
+    $PackagePath = [System.IO.Path]::GetFullPath((Join-Path (Get-Location).ProviderPath $PackagePath))
+}
+
+## Source Files
+[string[]] $SourceFiles = @(
+    "..\src\mainTemplate.json"
+    "..\src\createUiDefinition.json"
+)
+
+## Copy Files to Staging Directory
+[void](New-Item $StagingDirectory -ItemType Directory -ErrorAction SilentlyContinue)
+$StagingFiles = Copy-Item $SourceFiles -Destination $StagingDirectory -Force -PassThru -ErrorAction Stop
+
+## Zip Files in Staging Directory
+Add-Type -Assembly "System.IO.Compression.FileSystem"
+[System.IO.Compression.ZipFile]::CreateFromDirectory($StagingDirectory, $PackagePath)
+
+## Remove Staging Files
+Remove-Item $StagingFiles
